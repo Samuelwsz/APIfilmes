1cfa4b583765ea5e7f7fc69b9d49cb6a2f5d57be
samuelwsz <samuelxwsz@gmail.com>
2023-11-20 20:00:03 -0300

    att

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,36 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 import { VideoCameraIcon } from "@heroicons/react/24/solid"
-import { Link } from "react-router-dom"
+import { FormEvent, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function NavBar() {
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
   return (
     <header className="bg-black flex justify-between p-8  items-center flex-col md:flex-row">
-      <Link to="/" className="text-4xl text-center mb-4 md:mb-0 text-yellow-400">
+      <Link
+        to="/"
+        className="text-4xl text-center mb-4 md:mb-0 text-yellow-400"
+      >
         <VideoCameraIcon className="w-8 h-8" /> Movies Lib
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex mb-2 md:mb-0">
           <input
             className="p-2 mr-1 rounded-md w-full md:w-64 md:mr-2 outline-none"
             type="text"
             placeholder="Search movie"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <button type="submit" className="bg-white px-3 rounded-md">
             <MagnifyingGlassIcon className="w-5 h-5 text-black" />
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,56 @@
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+import { useEffect, useState } from "react"
+import { Movie } from "../interfaces/IMovie"
+import axios from "axios"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
 export default function Search() {
-  return <div>Search</div>
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [error, setError] = useState<string | null>(null)
+  const query = searchParams.get("q")
+
+  const getSearchedMovies = async (url: string) => {
+    try {
+      const res = await axios.get(url)
+      setMovies(res.data.results)
+      setError(null) // Limpa o estado de erro se a busca for bem-sucedida
+    } catch (error) {
+      console.error("Erro", error)
+      setError("Não foi possível encontrar filmes. Tente novamente.") // Define a mensagem de erro no estado
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+
+    getSearchedMovies(searchWithQueryURL)
+  }, [query])
+
+  return (
+    <div className="">
+      <h1 className="py-8 text-center text-3xl font-serif">
+        Bests movies : {query}
+      </h1>
+      {error && (
+        <p className="text-center text-xl text-red-500 font-semibold">
+          {error}
+        </p>
+      )}
+      {movies.length === 0 && (
+        <p className="text-center text-xl font-semibold">Loading...</p>
+      )}
+      <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-5 mb-5">
+        {movies.length > 0 &&
+          movies.map((movie) => {
+            return <MovieCard key={movie.id} movie={movie} />
+          })}
+      </div>
+    </div>
+  )
 }
