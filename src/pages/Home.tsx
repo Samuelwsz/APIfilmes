b5416bf74149ede96dffa1b643d36791b590ba19
samuelwsz <samuelxwsz@gmail.com>
2023-11-20 22:01:57 -0300

    att

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,14 +8,19 @@ const apiKey = import.meta.env.VITE_API_KEY
 
 export default function Home() {
   const [topMovies, setTopMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
 
   const getTopRatedMovies = async (url: string) => {
+    setLoading(true)
     try {
       const res = await axios.get(url)
       setTopMovies(res.data.results)
+      setError(null)
     } catch (error) {
-      console.error("Erro", error)
+      setError("Error")
     } finally {
+      setLoading(false)
     }
   }
 
@@ -28,9 +33,15 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="py-8 text-center text-3xl font-serif">Bests movies</h1>
-      {topMovies.length === 0 && (
-        <p className="text-center text-xl font-semibold">Loading...</p>
+      {loading && (
+        <p className=" text-3xl font-semibold text-black text-center">
+          Loading...
+        </p>
       )}
+      {error && (
+        <p className="text-red-500 font-semibold text-xl mt-2">{error}</p>
+      )}
+
       <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-5 mb-5">
         {topMovies.length > 0 &&
           topMovies.map((movie) => {
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -12,39 +12,51 @@ export default function Search() {
 
   const [movies, setMovies] = useState<Movie[]>([])
   const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [searched, setSearched] = useState<boolean>(false)
+
   const query = searchParams.get("q")
 
   const getSearchedMovies = async (url: string) => {
+    setLoading(true)
     try {
       const res = await axios.get(url)
       setMovies(res.data.results)
       setError(null) // Limpa o estado de erro se a busca for bem-sucedida
+      setSearched(true)
     } catch (error) {
-      console.error("Erro", error)
       setError("Não foi possível encontrar filmes. Tente novamente.") // Define a mensagem de erro no estado
     } finally {
+      setLoading(false)
     }
   }
 
   useEffect(() => {
-    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+    setSearched(true) // Define como true para a primeira renderização
+    window.history.replaceState(null, "", window.location.pathname)
+  }, [])
 
-    getSearchedMovies(searchWithQueryURL)
-  }, [query])
+  useEffect(() => {
+    if (searched && query) {
+      const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+      getSearchedMovies(searchWithQueryURL)
+    }
+  }, [query, searched])
 
   return (
-    <div className="">
+    <div>
       <h1 className="py-8 text-center text-3xl font-serif">
         Bests movies : {query}
       </h1>
-      {error && (
-        <p className="text-center text-xl text-red-500 font-semibold">
-          {error}
+      {loading && (
+        <p className="text-3xl font-semibold text-black text-center">
+          Loading...
         </p>
       )}
-      {movies.length === 0 && (
-        <p className="text-center text-xl font-semibold">Loading...</p>
+      {error && (
+        <p className="text-3xl font-semibold text-black text-center">{error}</p>
       )}
+
       <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-5 mb-5">
         {movies.length > 0 &&
           movies.map((movie) => {
